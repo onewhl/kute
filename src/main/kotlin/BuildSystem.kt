b8d8d3dd12fc56373bafa811c2c6eeb8aa9ac484
raipc <anton.rybochkin@axibase.com>
2022-11-14 22:45:14 +0100

    Fix and refactor BuildSystem module detection

--- src/main/kotlin/BuildSystem.kt
+++ src/main/kotlin/BuildSystem.kt
@@ -5,52 +5,48 @@ private val logger = KotlinLogging.logger {}
 
 enum class BuildSystem {
     GRADLE {
-        override fun getProjectModules(projectPath: String): Map<String, File> {
-            //TODO: process kts file as well
-            val file = File(projectPath, "settings.gradle")
-            return file.takeIf { file.isFile }.let {
-                "include\\s+'([^']+)'".toRegex()
-                    .findAll(file.readText())
-                    .map { it.groups[1]?.value ?: "" }
-                    .map { it to File(projectPath, it) }
-                    .toMap()
-                    .takeIf { it.isNotEmpty() }
-                    .also { logger.debug { "Found ${it?.size} modules in project $projectPath." } }
-                    ?: super.getProjectModules(projectPath)
-            }
+        private val moduleNameRegex = "include\\s+['\"]([^']+)['\"]".toRegex()
+
+        override fun getProjectModules(projectPath: File): Map<String, File> {
+            val settings = File(projectPath, "settings.gradle").takeIf { it.isFile }
+                ?: File(projectPath, "settings.gradle.kts").takeIf { it.isFile }
+            return settings?.let {
+                extractModuleNames(projectPath, settings, moduleNameRegex, 1)
+            } ?: super.getProjectModules(projectPath)
         }
     },
     MAVEN {
-        override fun getProjectModules(projectPath: String): Map<String, File> {
-            val file = File(projectPath, "pom.xml")
-            val regex = "(?<=<module>)[^<]*(?=</module>)".toRegex()
-            return regex.findAll(file.readText())
-                .map { it.groups[1]?.value ?: "" }
-                .map { it to File(projectPath, it) }
-                .toMap()
-                .takeIf { it.isNotEmpty() }
-                .also { logger.debug { "Found ${it?.size} modules in project $projectPath." } }
+        private val moduleNameRegex = "(?<=<module>)[^<]*(?=</module>)".toRegex()
+
+        override fun getProjectModules(projectPath: File): Map<String, File> {
+            val pomFile = File(projectPath, "pom.xml")
+            return extractModuleNames(projectPath, pomFile, moduleNameRegex, 0)
                 ?: super.getProjectModules(projectPath)
         }
     },
     ANT, OTHER;
 
-    open fun getProjectModules(projectPath: String): Map<String, File> =
-        File(projectPath).let { mapOf(it.name to it) }
+    open fun getProjectModules(projectPath: File): Map<String, File> =
+        mapOf(projectPath.name to projectPath)
             .also { logger.debug { "Found ${it.size} modules in project $projectPath." } }
 
     open fun getTestFilesRoot(projectPath: File) = projectPath
 }
 
-fun detectBuildSystem(projectDirPath: String): BuildSystem {
+private fun extractModuleNames(projectPath: File, settingsFile: File, regex: Regex, expectedGroup: Int): Map<String, File>? =
+    regex.findAll(settingsFile.readText())
+        .map { it.groups[expectedGroup]?.value ?: "" }
+        .map { it to File(projectPath, it) }
+        .toMap()
+        .takeIf { it.isNotEmpty() }
+        .also { logger.debug { "Found ${it?.size} modules in project $projectPath." } }
+
+fun detectBuildSystem(projectDirPath: File): BuildSystem {
     return when {
         File(projectDirPath, "build.gradle").isFile ||
-                File(projectDirPath, "build.gradle.kts").isFile -> {
-            BuildSystem.GRADLE
-        }
-        File(projectDirPath, "pom.xml").isFile -> {
-            BuildSystem.MAVEN
-        }
+                File(projectDirPath, "build.gradle.kts").isFile -> BuildSystem.GRADLE
+        File(projectDirPath, "pom.xml").isFile -> BuildSystem.MAVEN
+        File(projectDirPath, "build.xml").isFile -> BuildSystem.ANT
         else -> BuildSystem.OTHER
     }.also { logger.debug { "Detected build system in $projectDirPath is ${it.name}." } }
 }
