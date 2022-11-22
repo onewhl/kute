1caaee5b54840be3adcc47f3f1da4d35af9b4d88
raipc <anton.rybochkin@axibase.com>
2022-11-22 17:59:00 +0100

    Fixed module paths recognition in multi-module Gradle projects

---
+++ src/test/kotlin/BuildSystemTest.kt
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+class BuildSystemTest {
+    @Test
+    fun testDetectGradleModules(@TempDir projectPath: File) {
+        val settings = """
+            rootProject.name = "solr-root"
+
+            include "project:projectj"
+            include "project:core"
+            include "project:server"
+        """.trimIndent()
+        File(projectPath, "settings.gradle").writeText(settings)
+        val resolvedModules = BuildSystem.GRADLE.getProjectModules(projectPath)
+        val expectedModules = mapOf(
+            "project:projectj" to File(projectPath, "project/projectj"),
+            "project:core" to File(projectPath, "project/core"),
+            "project:server" to File(projectPath, "project/server"),
+            )
+        assertEquals(expectedModules, resolvedModules)
+    }
+}
