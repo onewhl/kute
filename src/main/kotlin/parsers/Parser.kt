fc73fc399c15dafea6e8e82222522ea32d0076cb
raipc <anton.rybochkin@axibase.com>
2022-12-13 12:41:28 +0100

    Move createParser method to Parser.kt

--- src/main/kotlin/parsers/Parser.kt
+++ src/main/kotlin/parsers/Parser.kt
@@ -1,9 +1,15 @@
 package parsers
 
+import ModuleInfo
 import TestMethodInfo
 import java.io.File
 
 interface Parser {
     fun process(files: List<File>): List<TestMethodInfo>
     val language: Lang
+}
+
+fun createParser(lang: Lang, path: File, module: ModuleInfo, classNameToFile: Map<String, List<File>>) = when (lang) {
+    Lang.JAVA -> JavaTestParser(path, module, classNameToFile)
+    Lang.KOTLIN -> KotlinTestParser(path, module, classNameToFile)
 }
