dccdcc84b7f2730fdf1722f86552f8784ca48f05
raipc <anton.rybochkin@axibase.com>
2022-12-26 19:02:49 +0100

    task executor simplification

--- src/main/kotlin/TaskExecutor.kt
+++ src/main/kotlin/TaskExecutor.kt
@@ -1,7 +1,7 @@
 import java.util.concurrent.Callable
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ForkJoinPool
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.ForkJoinTask
 import kotlin.math.max
 
 class TaskExecutor(ioThreads: Int, computeThreads: Int) {
@@ -9,36 +9,10 @@ class TaskExecutor(ioThreads: Int, computeThreads: Int) {
     private val cpuExecutor = if (computeThreads == 0) ForkJoinPool.commonPool() else ForkJoinPool(
         max(computeThreads - 1, 1)
     )
-    private val resultSaveExecutor = ForkJoinPool(1)
 
     fun <T : Any?> runDownloadingTask(task: Callable<T>): CompletableFuture<T> =
         CompletableFuture.supplyAsync(task::call, ioExecutor)
 
-    fun <T : Any?> runComputationTask(task: Callable<T>): CompletableFuture<T> =
-        CompletableFuture.supplyAsync(task::call, cpuExecutor)
-
-    fun runResultSavingTask(task: Runnable) {
-        resultSaveExecutor.execute(task)
-    }
-
-    fun join() {
-        for (i in (0..2)) {
-            do {
-                var quiescent = cpuExecutor.awaitQuiescence(Long.MAX_VALUE, TimeUnit.MILLISECONDS)
-                if (ioExecutor == ForkJoinPool.commonPool()) {
-                    quiescent = ioExecutor.awaitQuiescence(Long.MAX_VALUE, TimeUnit.MILLISECONDS) && quiescent
-                } else if (!ioExecutor.isQuiescent) {
-                    quiescent = false
-                }
-                if (!resultSaveExecutor.isQuiescent) {
-                    quiescent = false
-                }
-                if (!quiescent) {
-                    Thread.sleep(100)
-                }
-            } while (!quiescent)
-            Thread.sleep(100)
-        }
-    }
-
+    fun <T : Any?> runComputationTask(task: Callable<T>): ForkJoinTask<T> =
+        cpuExecutor.submit(task)
 }
